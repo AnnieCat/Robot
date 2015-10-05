ba3148d47f99ae0d50950f83045abd16f7bcef1c
AnnieCat <annieanddoughnut@gmail.com>
2015-10-05 13:16:13 -0700

    Now I have Visual Studio controlling the X and Y axis of the Servo PanTilt motor

--- ArduinoRecieve/ArduinoRecieve.ino
+++ ArduinoRecieve/ArduinoRecieve.ino
@@ -4,25 +4,40 @@
 #include <midi_Namespace.h>
 #include <midi_Settings.h>
 
+#include <Servo.h>
+
+Servo myServoX;
+Servo myServoY;
+
+int myX;
+int myY;
+
+
 MIDI_CREATE_DEFAULT_INSTANCE();
 
 void HandleNoteOn(byte channel, byte pitch, byte velocity)
-{     
-  if(pitch == 0)
-    digitalWrite(12, LOW);
-  if(pitch == 1)
-    digitalWrite(12, HIGH);
+{  
+  if(channel == 1)
+  {
+    myX = map(velocity, 1, 127, 10, 180);
+    myServoX.write(myX);
+  }   
+  
+  if(channel == 2)
+  {
+    myY = map(velocity, 1, 127, 10, 180);
+    myServoY.write(myY);
+  }
 }
 
 void setup() {
   MIDI.begin(MIDI_CHANNEL_OMNI);
   MIDI.setHandleNoteOn(HandleNoteOn);
+  
+  myServoX.attach(9);
+  myServoY.attach(10);
 
   pinMode(12, OUTPUT);
-
-  digitalWrite(12, HIGH);
-  delay(500);
-  digitalWrite(12, LOW);
 }
 
 void loop() {
--- VS_to_arduino/SendSignal.cpp
+++ VS_to_arduino/SendSignal.cpp
@@ -1,14 +1,146 @@
 #include "Midi.h"
 #include <chrono>
 #include <thread>
+
 #include <iostream>
 #include <queue>
-
 #include <memory>
+#include <string>
+
+#include "pxcsensemanager.h"
+#include "pxcfaceconfiguration.h"
 
 using namespace std;
 
-int main(){
+
+class SendSignal
+{
+public:
+	//Constructor and Destructor
+	SendSignal(){	}
+	~SendSignal(){	}
+	void setup();
+	void update();
+	void cleanup();
+
+	int headTurnModifier;
+
+private:
+	PXCSenseManager *mSenseMgr;
+	PXCFaceData *mFaceData;
+};
+
+void SendSignal::setup()
+{
+
+
+	mSenseMgr = PXCSenseManager::CreateInstance();
+	auto st = mSenseMgr->EnableFace();
+	auto fm = mSenseMgr->QueryFace();
+	auto cfg = fm->CreateActiveConfiguration();
+	st = mSenseMgr->Init();
+
+	cfg->pose.isEnabled = false;
+	cfg->ApplyChanges();
+	mFaceData = fm->CreateOutput();
+}
+
+//face expressions-- Anger, Disgust, Fear, Joy, Sadness, Surprise
+//use expression configuration interface
+//https://software.intel.com/sites/landingpage/realsense/camera-sdk/v1.1/documentation/html/expressionsconfiguration_pxcfaceconfiguration.html
+//https://software.intel.com/sites/landingpage/realsense/camera-sdk/v1.1/documentation/html/queryexpressions_face_pxcfacedata.html
+
+void SendSignal::update()
+{
+	MidiPlayer player;
+
+	while(true)
+	{
+		int myx;
+		int mychannel;
+
+		cout << "channel? " << endl;
+		cin >> mychannel;
+		cout << "value?  10-120 for x, 130 - 254 for y " << endl;
+		cin >> myx;
+		player.SendMidiMessage(mychannel, 10, myx);
+
+		//send 10 - 120
+		
+	}
+
+	/*if (mSenseMgr->AcquireFrame(true) >= PXC_STATUS_NO_ERROR)
+	{
+		if (!mFaceData)
+			mFaceData = mSenseMgr->QueryFace()->CreateOutput();
+		if (mFaceData)
+		{
+			mFaceData->Update();
+			auto numFaces = mFaceData->QueryNumberOfDetectedFaces();
+			
+			for (int i = 0; i < numFaces; ++i)
+			{
+				//track up to 4 faces -- only 1 face has landmarks
+				auto foundFace = mFaceData->QueryFaceByIndex(i);
+				if (foundFace)
+				{
+					auto dd = foundFace->QueryDetection();
+					if (dd)
+					{
+						PXCRectI32 outR;
+						if (dd->QueryBoundingRect(&outR))
+						{
+							MidiPlayer player;
+							int myx;
+							int myy;
+
+							cout << "My X: " << outR.x << " My Y: " << outR.y << endl;
+							
+							cout << "x coordinate? " << endl;
+							cin >> myx;
+							player.SendMidiMessage(0, myx, 10);
+
+							cout << "y coordinate? " << endl;
+							cin >> myy;
+							player.SendMidiMessage(1, myy, 10);
+
+
+
+							//player.SendMidiMessage(0, lightbulb, 10);
+							//std::this_thread::sleep_for(std::chrono::milliseconds(7));
+						}
+					}
+				}
+			}
+		}
+		mSenseMgr->ReleaseFrame();
+	}*/
+}
+
+void SendSignal::cleanup()
+{
+	if (mFaceData)
+		mFaceData->Release();
+	mSenseMgr->Close();
+}
+
+int main()
+{
+	SendSignal mySendSignal;
+
+	mySendSignal.setup();
+
+	while (true)
+		mySendSignal.update();
+	mySendSignal.cleanup();
+}
+
+// C:\Program Files (x86)\Intel\RSSDK\lib\$(Platform)
+// $(GrapheneTrimpin)\third_party\lib\$(Platform)\
+
+//  Sample Code -- C++ talking to Arduino
+
+/*int main(){
 
 	MidiPlayer player;
 
@@ -22,4 +154,4 @@ int main(){
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(7));
 	}
-}
+}*/

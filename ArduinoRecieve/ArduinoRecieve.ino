9a0c057a340d626feed66016aed2fbc6357829b9
AnnieCat <annieanddoughnut@gmail.com>
2015-10-05 15:41:43 -0700

    I got the robot following you.
    Now I need to fix the fact that it snaps to center when it loses you.  But... score.

--- ArduinoRecieve/ArduinoRecieve.ino
+++ ArduinoRecieve/ArduinoRecieve.ino
@@ -19,13 +19,12 @@ void HandleNoteOn(byte channel, byte pitch, byte velocity)
 {  
   if(channel == 1)
   {
-    myX = map(velocity, 1, 127, 10, 180);
+    myX = velocity + pitch;
     myServoX.write(myX);
-  }   
-  
+  }
   if(channel == 2)
   {
-    myY = map(velocity, 1, 127, 10, 180);
+    myY = velocity + pitch;
     myServoY.write(myY);
   }
 }
@@ -36,6 +35,8 @@ void setup() {
   
   myServoX.attach(9);
   myServoY.attach(10);
+
+ // myServoY.write(80);
 
   pinMode(12, OUTPUT);
 }
--- VS_to_arduino/SendSignal.cpp
+++ VS_to_arduino/SendSignal.cpp
@@ -23,7 +23,6 @@ public:
 	void update();
 	void cleanup();
 
-	int headTurnModifier;
 
 private:
 	PXCSenseManager *mSenseMgr;
@@ -45,6 +44,11 @@ void SendSignal::setup()
 	mFaceData = fm->CreateOutput();
 }
 
+int Map(int value, int low1, int high1, int low2, int high2)
+{
+	return low2 + (value - low1) * (high2 - low2) / (high1 - low1);
+}
+
 //face expressions-- Anger, Disgust, Fear, Joy, Sadness, Surprise
 //use expression configuration interface
 //https://software.intel.com/sites/landingpage/realsense/camera-sdk/v1.1/documentation/html/expressionsconfiguration_pxcfaceconfiguration.html
@@ -52,24 +56,26 @@ void SendSignal::setup()
 
 void SendSignal::update()
 {
+	// ---- Send explicit X Y coordinates
+		
 	MidiPlayer player;
 
-	while(true)
+	/*while(true)
 	{
 		int myx;
 		int mychannel;
 
-		cout << "channel? " << endl;
+		cout << "channel? X is 1, Y is 2" << endl;
 		cin >> mychannel;
 		cout << "value?  10-120 for x, 130 - 254 for y " << endl;
 		cin >> myx;
-		player.SendMidiMessage(mychannel, 10, myx);
+		player.SendMidiMessage(mychannel, 0, myx);
 
 		//send 10 - 120
 		
-	}
+	}*/
 
-	/*if (mSenseMgr->AcquireFrame(true) >= PXC_STATUS_NO_ERROR)
+	if (mSenseMgr->AcquireFrame(true) >= PXC_STATUS_NO_ERROR)
 	{
 		if (!mFaceData)
 			mFaceData = mSenseMgr->QueryFace()->CreateOutput();
@@ -91,31 +97,62 @@ void SendSignal::update()
 						if (dd->QueryBoundingRect(&outR))
 						{
 							MidiPlayer player;
-							int myx;
-							int myy;
 
-							cout << "My X: " << outR.x << " My Y: " << outR.y << endl;
+							int myX, myY;
 							
-							cout << "x coordinate? " << endl;
-							cin >> myx;
-							player.SendMidiMessage(0, myx, 10);
+							// for the servo's extent extend last 2 digits to 180 and 10
+							myX = Map(outR.x, 0, 1500, 130, 60);
+							myY = Map(outR.y, 0, 730, 40, 80);
 
-							cout << "y coordinate? " << endl;
-							cin >> myy;
-							player.SendMidiMessage(1, myy, 10);
+							cout << "Rotation Value: " << myX << endl;
+							if (myX >= 60 && myX <= 130)
+							{
+								float temp = myX / 126;
 
+								if (temp < 1)
+								{
+									player.SendMidiMessage(1, 0, myX);
+									cout << "0 - " << myX << endl;
+									cout << "rotation: " << myX << endl;
+								}
+								else
+								{
+									int modulo = myX % 126;
+									player.SendMidiMessage(1, modulo, 126);
+									cout << "1 - " << modulo << endl;
+									cout << "rotation: " << myX << endl;
+								}
+							}
+							
+							if (myY >= 40 && myY <= 80)
+							{
+								float temp = myY / 126;
 
+								if (temp < 1)
+								{
+									player.SendMidiMessage(2, 0, myY);
+									cout << "0 - " << myY << endl;
+									cout << "rotation: " << myY << endl;
+								}
+								else
+								{
+									int modulo = myY % 126;
+									player.SendMidiMessage(2, modulo, 126);
+									cout << "1 - " << modulo << endl;
+									cout << "rotation: " << myY << endl;
+								}
+							}
 
-							//player.SendMidiMessage(0, lightbulb, 10);
-							//std::this_thread::sleep_for(std::chrono::milliseconds(7));
-						}
+							std::this_thread::sleep_for(std::chrono::milliseconds(50));}
 					}
 				}
 			}
 		}
 		mSenseMgr->ReleaseFrame();
-	}*/
+	}
 }
+
+
 
 void SendSignal::cleanup()
 {

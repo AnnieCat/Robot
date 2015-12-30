864c364246175efb0f1bfb0d2e4b684d24c10d2f
AnnieCat <annieanddoughnut@gmail.com>
2015-12-30 15:21:48 -0800

    newest Robot Code, trying to commit it.

--- VS_to_Arduino/main.cpp
+++ VS_to_Arduino/main.cpp
@@ -53,51 +53,51 @@ vector<pair<string, int>> BlinkAnim = {
 vector<pair<string, int>> SupriseAnim = {
 	{ "../textures/neutral/eyeOpen.jpg", 0 },
 	{ "../textures/suprise/suprise_4.jpg", 2 },
-	{ "../textures/suprise/suprise_3-5.jpg", 18 },
-	{ "../textures/suprise/suprise_3.jpg", 20 },
-	{ "../textures/suprise/close_2.jpg", 37 },
-	{ "../textures/suprise/close_3.jpg", 41 },
-	{ "../textures/suprise/close_4.jpg", 45 },
-	{ "../textures/suprise/close_5.jpg", 49 },
-	{ "../textures/suprise/close_6.jpg", 53 },
-	{ "../textures/suprise/close_7.jpg", 57 },
-	{ "../textures/suprise/close_7.jpg", 210 },
-	{ "../textures/wink/wink_05.jpg", 212 },
-	{ "../textures/wink/wink_04.jpg", 214 },
-	{ "../textures/wink/wink_03.jpg", 216 },
-	{ "../textures/wink/wink_02.jpg", 218 },
-	{ "../textures/neutral/eyeOpen.jpg", 220 },
-	{ "../textures/wink/wink_02.jpg", 222 },
-	{ "../textures/wink/wink_02.jpg", 224 },
-	{ "../textures/wink/wink_04.jpg", 226 },
-	{ "../textures/wink/wink_05.jpg", 228 },
-	{ "../textures/wink/wink_04.jpg", 230 },
-	{ "../textures/wink/wink_02.jpg", 232 },
-	{ "../textures/wink/wink_02.jpg", 234 },
-	{ "../textures/neutral/eyeOpen.jpg", 236 },
-	{ "../textures/neutral/eyeOpen.jpg", 260 },
-	{ "../textures/suprise/lookR_02.jpg", 262 },
-	{ "../textures/suprise/lookR_03.jpg", 264 },
-	{ "../textures/suprise/lookR_04.jpg", 266 },
-	{ "../textures/suprise/lookR_05.jpg", 268 },
-	{ "../textures/suprise/lookR_05.jpg", 353 },
-	{ "../textures/suprise/lookR_04.jpg", 355 },
-	{ "../textures/suprise/lookR_03.jpg", 357 },
-	{ "../textures/suprise/lookR_02.jpg", 359 },
-	{ "../textures/neutral/eyeOpen.jpg", 361 },
-	{ "../textures/suprise/lookL_02.jpg", 363 },
-	{ "../textures/suprise/lookL_03.jpg", 365 },
-	{ "../textures/suprise/lookL_04.jpg", 367 },
-	{ "../textures/suprise/lookL_05.jpg", 369 },
-	{ "../textures/suprise/lookL_05.jpg", 469 },
-	{ "../textures/suprise/lookL_04-5.jpg", 473 },
-	{ "../textures/suprise/lookL_04.jpg", 477 },
-	{ "../textures/suprise/lookL_03-5.jpg", 481 },
-	{ "../textures/suprise/lookL_03.jpg", 484 },
-	{ "../textures/suprise/lookL_02-5.jpg", 488 },
-	{ "../textures/suprise/lookL_02.jpg", 492 },
-	{ "../textures/suprise/lookL_01-5.jpg", 498 },
-	{ "../textures/neutral/eyeOpen.jpg", 501 }
+	{ "../textures/suprise/suprise_3-5.jpg", 16 },  
+	{ "../textures/suprise/suprise_3.jpg", 2 },   
+	{ "../textures/suprise/close_2.jpg", 17 },		
+	{ "../textures/suprise/close_3.jpg", 4 },		
+	{ "../textures/suprise/close_4.jpg", 4 },		
+	{ "../textures/suprise/close_5.jpg", 4 },		
+	{ "../textures/suprise/close_6.jpg", 4 },		
+	{ "../textures/suprise/close_7.jpg", 4 },		
+	{ "../textures/suprise/close_7.jpg", 153 },		
+	{ "../textures/wink/wink_05.jpg", 2 },		
+	{ "../textures/wink/wink_04.jpg", 2 },		
+	{ "../textures/wink/wink_03.jpg", 2 },		
+	{ "../textures/wink/wink_02.jpg", 2 },		
+	{ "../textures/neutral/eyeOpen.jpg", 2 },	
+	{ "../textures/wink/wink_02.jpg", 2 },		
+	{ "../textures/wink/wink_02.jpg", 2 },		
+	{ "../textures/wink/wink_04.jpg", 2 },		
+	{ "../textures/wink/wink_05.jpg", 2 },		
+	{ "../textures/wink/wink_04.jpg", 2 },		
+	{ "../textures/wink/wink_02.jpg", 2 },		
+	{ "../textures/wink/wink_02.jpg", 2 },		
+	{ "../textures/neutral/eyeOpen.jpg", 2 },		
+	{ "../textures/neutral/eyeOpen.jpg", 24 },		
+	{ "../textures/suprise/lookR_02.jpg", 2 },	
+	{ "../textures/suprise/lookR_03.jpg", 2 },	
+	{ "../textures/suprise/lookR_04.jpg", 2 },	
+	{ "../textures/suprise/lookR_05.jpg", 2 },	
+	{ "../textures/suprise/lookR_05.jpg", 85 },	
+	{ "../textures/suprise/lookR_04.jpg", 2 },	
+	{ "../textures/suprise/lookR_03.jpg", 2 },	
+	{ "../textures/suprise/lookR_02.jpg", 2 },	
+	{ "../textures/neutral/eyeOpen.jpg", 2 },	
+	{ "../textures/suprise/lookL_02.jpg", 2 },	
+	{ "../textures/suprise/lookL_03.jpg", 2 },	
+	{ "../textures/suprise/lookL_04.jpg", 2 },	
+	{ "../textures/suprise/lookL_05.jpg", 2 },	
+	{ "../textures/suprise/lookL_05.jpg", 100 },
+	{ "../textures/suprise/lookL_04-5.jpg", 4 },
+	{ "../textures/suprise/lookL_04.jpg", 4 },	
+	{ "../textures/suprise/lookL_03-5.jpg", 4 },
+	{ "../textures/suprise/lookL_03.jpg", 3 },	
+	{ "../textures/suprise/lookL_02-5.jpg", 4 },
+	{ "../textures/suprise/lookL_02.jpg", 4 },	
+	{ "../textures/suprise/lookL_01-5.jpg", 4 },
+	{ "../textures/neutral/eyeOpen.jpg", 3 }	
 
 };
 #pragma endregion
@@ -114,7 +114,7 @@ public:
 	void setup();
 	void timerUpdate(float timestep);
 	void drawFrame(int, int);
-	void loadSequence(vector<pair<string, int>>);
+	//void loadSequence(vector<pair<string, int>>);
 	void playSequence(vector<pair<string, int>>);
 	void sendEmotion(string);
 	void cleanup();
@@ -195,29 +195,21 @@ int main()
 	PXCFaceData::ExpressionsData::FaceExpressionResult raiseLeftBrow;
 	PXCFaceData::ExpressionsData::FaceExpressionResult raiseRightBrow;
 	PXCFaceData::ExpressionsData::FaceExpressionResult openMouth;
-
-	mySignal.loadSequence(BlinkAnim);
-
+	
 #pragma endregion
 
 	while (!glfwWindowShouldClose(win))
 	{
 		glfwPollEvents();
 
-
 		const double t1 = glfwGetTime();
 		const float timestep = (float)(t1 - t0);
 		t0 = t1;
-
-		// Today's lessons:
-		// 1) Sometimes if shit is really fucked up, do a build -> clean solution (Typically if you've been messing around with project settings and stuff is refusing to compile)
-		// 2) Use glfwGetTime, or std::chrono::high_resolution_clock (Google it if you want to learn more)
-		// 3) If you have complex objects to construct, consider pulling them outside a loop. Some objects might take 1/4 of a second to construct, and will kill your time critical loop.
+		
 		mySignal.timerUpdate(timestep);
 		
 		// update camera information here
-
-/*#pragma region Perceptual
+#pragma region Perceptual
 
 		int numFaces = 0;
 		
@@ -266,7 +258,7 @@ int main()
 					edata->QueryExpression(PXCFaceData::ExpressionsData::EXPRESSION_MOUTH_OPEN, &openMouth);
 
 #pragma endregion
-
+					
 #pragma region Sending Commands
 					//suprise
 					if (raiseLeftBrow.intensity > 80 && raiseRightBrow.intensity > 80 && openMouth.intensity > 30)
@@ -287,7 +279,7 @@ int main()
 #pragma endregion
 		}
 
-#pragma endregion*/
+#pragma endregion
 
 		// All Output goes Here- rendering, commands to arduino
 		glfwGetFramebufferSize(win, &width, &height);
@@ -301,7 +293,6 @@ int main()
 		glOrtho(0, width, height, 0, -1, +1);
 		
 		mySignal.drawFrame(width, height);
-		
 
 		glfwSwapBuffers(win);
 	}
@@ -310,12 +301,15 @@ int main()
 	mySignal.cleanup();
 }
 
+
+
 void SendSignal::sendEmotion(string myEmo)
 {
 	if (myEmo == "suprise")
 	{
 		cout << "suprise" << endl;
-		//playSequence(SupriseAnim);
+		playSequence(SupriseAnim);
+		AudioPlay("suprise.wav");
 	}
 }
 
@@ -324,16 +318,18 @@ void SendSignal::timerUpdate(float timestep)
 {
 	//This is our Blink release
 	millisec += timestep;
+	cout << millisec << endl;
 
 	if (millisec > blinkVal)
 	{
 		if (neutral)
 		{
 			AudioPlay("blink.wav");
+			//AudioPlay("suprise.wav");
 			cout << "blink!" << endl;
 			cout << blinkVal << endl;
-			//playSequence(BlinkAnim);
-			playSequence(SupriseAnim);
+			playSequence(BlinkAnim);
+			//playSequence(SupriseAnim);
 
 			blinkVal = 6 + (rand() % 10);
 			millisec = 0;
@@ -350,7 +346,6 @@ void SendSignal::drawFrame(int windowWidth, int windowHeight)
 {
 	int resetFrameCount;
 	
-	//cout << "time: " << glfwGetTime() << "               frames: "<< (int)(glfwGetTime() * 60) << endl;
 
 	// If there is an animation and we have waited the appropriate number of frames
 	if (anim_image_index < anim.size() && anim_frame_count == (anim[anim_image_index].second))
@@ -358,6 +353,7 @@ void SendSignal::drawFrame(int windowWidth, int windowHeight)
 		
 		// Load the next image from the animation
 		myImage = stbi_load(anim[anim_image_index].first.c_str(), &width, &height, nullptr, 3);
+		
 
 		++anim_image_index;
 		anim_frame_count = 0;
@@ -373,19 +369,11 @@ void SendSignal::drawFrame(int windowWidth, int windowHeight)
 	glPixelZoom(1, -1);
 	glDrawPixels(800, 480, GL_RGB, GL_UNSIGNED_BYTE, myImage);
 
-	//anim_frame_count = (int)(glfwGetTime() * 60);
+
 	++anim_frame_count;
 	
 }
 
-void SendSignal::loadSequence(vector<pair<string, int>> xxAnim)
-{
-	int xnumber = xxAnim.size();
-	for (int i = 0; i < xnumber; i++)
-	{
-		cout << xxAnim[i].first << endl;
-	}
-}
 
 void SendSignal::playSequence(vector<pair<string, int>> xxAnim)
 {
